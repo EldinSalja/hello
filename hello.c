2a9673e059491c251e18d7d286f54d1261429add
EldinSalja <esalja2@huskers.unl.edu>
2022-08-26 23:54:51 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
  int main(int argc, char **argv) {
 
         printf("Hello, I'm Eldin Salja!\n");
-        printf("Major is Computer Science!\n");
+        printf("My major is Computer Science!\n");
 
         return 0;
 }
