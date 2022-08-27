c7b95f4d7a0854965030062b539ff206f1ff10fd
bwormsley <bowrmsley2@huskers.unl.edu>
2022-08-27 20:24:04 +0000

    added my info

--- hello.c
+++ hello.c
@@ -5,7 +5,9 @@
  int main(int argc, char **argv) {
 
         printf("Hello, I'm Eldin Salja!\n");
+        printf("And I'm Benjamin Wormsley!"\n);
         printf("Major is Computer Science!\n");
+        printf("My major is Computer Engineering!");
 
         return 0;
 }
