3cce53d4ca031b55984696a9f26ff5549c4bbe7a
EldinSalja <esalja2@huskers.unl.edu>
2022-08-27 00:32:40 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
  int main(int argc, char **argv) {
 
         printf("Hello, I'm Eldin Salja!\n");
-        printf("My major is Computer Science!\n");
+        printf("Major is Computer Science!\n");
 
         return 0;
 }
